f8b72999f0b76c8b3a8b1f284a0d1b8619ec547c
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-06-10 00:30:11 +0900

    LogInForm

--- lib/log_in/log_in_screen.dart
+++ lib/log_in/log_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_carrot_market/home/home_screen.dart';
 import 'package:flutter_carrot_market/log_in/kakao_login.dart';
+import 'package:flutter_carrot_market/log_in/log_in_form.dart';
 import 'package:flutter_carrot_market/sign_up/change_color_button.dart';
 
 class LogInScreen extends StatefulWidget {
@@ -58,67 +59,35 @@ class _LogInScreenState extends State<LogInScreen> {
                 const SizedBox(
                   height: 20,
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: '이메일',
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value != null && value.isEmpty) {
-                      return "이메일을 제대로 입력해주세요";
-                    }
-                    return null;
-                  },
-                  onSaved: (newValue) {
+                LogInForm(
+                  logInHint: '이메일',
+                  eyePass: null,
+                  secret: false,
+                  saveValue: (newValue) {
                     if (newValue != null) {
                       formData['email'] = newValue;
                     }
                   },
+                  logInKeyBoard: TextInputType.emailAddress,
                 ),
                 const SizedBox(
                   height: 16,
                 ),
-                TextFormField(
-                  obscureText: obscureText,
-                  decoration: InputDecoration(
-                    suffix: GestureDetector(
-                      onTap: toggleObscureText,
-                      child: obscureText
-                          ? const Icon(Icons.open_in_new)
-                          : const Icon(Icons.open_in_new_off),
-                    ),
-                    hintText: '비밀번호',
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
+                LogInForm(
+                  logInHint: '비밀번호',
+                  eyePass: GestureDetector(
+                    onTap: toggleObscureText,
+                    child: obscureText
+                        ? const Icon(Icons.open_in_new)
+                        : const Icon(Icons.open_in_new_off),
                   ),
-                  validator: (value) {
-                    if (value != null && value.isEmpty) {
-                      return "비밀번호를 제대로 입력해주세요";
-                    }
-                    return null;
-                  },
-                  onSaved: (newValue) {
+                  secret: obscureText,
+                  saveValue: (newValue) {
                     if (newValue != null) {
                       formData['password'] = newValue;
                     }
                   },
+                  logInKeyBoard: TextInputType.text,
                 ),
                 const SizedBox(
                   height: 28,
