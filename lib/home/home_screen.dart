b7cad6d82ea80ee10758feb9be6b1219ad243cb7
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-07-04 21:54:43 +0900

    FloatingActionButton 구현

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_scrolling_fab_animated/flutter_scrolling_fab_animated.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, String>> datas = [];
+  final ScrollController _scrollController = ScrollController();
+  bool onTop = true;
 
   @override
   void initState() {
@@ -20,51 +23,78 @@ class _HomeScreenState extends State<HomeScreen> {
             "https://static.luck-d.com/product/1732/main_carousel/NIKE_AIR_JORDAN_1_HIGH_85_BLACK_WHITE_BQ4422001_45800.jpg",
         "title": "나이키 에어 조던 1 하이 85 블랙 화이트",
         "location": "자양동",
-        "price": "120000원",
+        "price": "120,000원",
       },
       {
         "image":
             "https://cdn.weekly.chosun.com/news/photo/202208/21784_42359_1424.jpg",
         "title": "아이폰14프로",
         "location": "능동",
-        "price": "500000원",
+        "price": "500,000원",
       },
       {
         "image":
             "https://image.msscdn.net/images/goods_img/20210428/1926034/1926034_1_500.jpg",
         "title": "검정색 반바지",
         "location": "구의동",
-        "price": "10000원",
+        "price": "10,000원",
       },
       {
         "image":
             "https://image.msscdn.net/images/goods_img/20200911/1596890/1596890_1_500.jpg",
         "title": "노이즈캔슬링 블루투스 헤드폰",
         "location": "화양동",
-        "price": "100000원",
+        "price": "100,000원",
       },
       {
         "image":
             "https://image.msscdn.net/images/goods_img/20220125/2326935/2326935_11_500.jpg",
         "title": "예일 후드티",
         "location": "자양동",
-        "price": "30000원",
+        "price": "30,000원",
       },
       {
         "image":
             "https://image.msscdn.net/images/goods_img/20220704/2645649/2645649_1_500.jpg",
         "title": "무브먼트 플라워 미드나잇블루 티셔츠 화이트",
         "location": "자양동",
-        "price": "15000원",
+        "price": "15,000원",
       },
       {
         "image":
             "https://image.msscdn.net/images/goods_img/20220331/2458631/2458631_1_500.jpg",
         "title": "RC BB557 ORANGE 선글라스 ",
         "location": "자양동",
-        "price": "5000원",
+        "price": "5,000원",
+      },
+      {
+        "image":
+            "https://image.msscdn.net/images/goods_img/20220331/2458631/2458631_1_500.jpg",
+        "title": "RC BB557 ORANGE 선글라스 ",
+        "location": "자양동",
+        "price": "5,000원",
+      },
+      {
+        "image":
+            "https://image.msscdn.net/images/goods_img/20220331/2458631/2458631_1_500.jpg",
+        "title": "RC BB557 ORANGE 선글라스 ",
+        "location": "자양동",
+        "price": "5,000원",
+      },
+      {
+        "image":
+            "https://image.msscdn.net/images/goods_img/20220331/2458631/2458631_1_500.jpg",
+        "title": "RC BB557 ORANGE 선글라스 ",
+        "location": "자양동",
+        "price": "5,000원",
       },
     ];
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
@@ -102,6 +132,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: ListView.separated(
+        controller: _scrollController,
         padding: const EdgeInsets.symmetric(horizontal: 15),
         // physics: const ClampingScrollPhysics(),
         itemBuilder: (BuildContext context, int index) {
@@ -165,6 +196,26 @@ class _HomeScreenState extends State<HomeScreen> {
             color: Colors.black.withOpacity(0.1),
           );
         },
+      ),
+      floatingActionButton: ScrollingFabAnimated(
+        width: 100,
+        height: 40,
+        icon: const FaIcon(
+          FontAwesomeIcons.plus,
+          color: Colors.white,
+          size: 11,
+        ),
+        text: const Text(
+          '글쓰기',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPress: () {},
+        color: Colors.amber.shade900,
+        scrollController: _scrollController,
+        animateIcon: false,
+        inverted: false,
       ),
     );
   }
